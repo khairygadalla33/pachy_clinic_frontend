288212a3bdbcb710e838c90e87159c4e049b3f5e
Khairy <RECAVE.EG@GMAIL.COM>
2026-08-01 19:10:34 +0300

    fix: add purpose to PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,14 @@ export default defineConfig({
           {
             src: 'favicon.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'favicon.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
