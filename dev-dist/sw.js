ce8cea5094d3209e288109e6d6bd42924cd045a4
Khairy <RECAVE.EG@GMAIL.COM>
2026-08-05 02:15:05 +0300

    Update before production

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.3ja3utj0b3s"
+    "revision": "0.u62chb27ng8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
